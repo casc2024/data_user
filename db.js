3afb83def40254df20863343db2b840b4c7aaf83
casc2024 <casc2024@gmail.com>
2020-02-22 18:12:42 -0500

    Create db.js

---
+++ db.js
@@ -0,0 +1,227 @@
+[
+  {
+    "_id": "5e51b4939c229a37d9e7ea76",
+    "index": 0,
+    "guid": "4d5fbce1-5c7f-4238-8569-d66dd6bca82f",
+    "isActive": true,
+    "balance": "$1,540.17",
+    "picture": "http://placehold.it/32x32",
+    "age": 36,
+    "eyeColor": "green",
+    "name": "Huffman Spears",
+    "gender": "male",
+    "company": "RODEOCEAN",
+    "email": "huffmanspears@rodeocean.com",
+    "phone": "+1 (831) 546-2589",
+    "address": "643 Rockaway Parkway, Hiseville, Texas, 2568",
+    "about": "Ut sunt officia cupidatat proident consequat dolore nostrud exercitation esse ex in. Mollit mollit reprehenderit exercitation mollit fugiat ad veniam deserunt ipsum consectetur id. Incididunt consectetur ex ut eu laborum consequat. Minim irure laborum pariatur enim enim eu id qui.\r\n",
+    "registered": "2015-09-11T06:00:10 +05:00",
+    "latitude": 81.736704,
+    "longitude": -135.327502,
+    "tags": [
+      "aliqua",
+      "deserunt",
+      "laboris",
+      "minim",
+      "ipsum",
+      "dolor",
+      "dolore"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Anthony Rose"
+      },
+      {
+        "id": 1,
+        "name": "Genevieve Hutchinson"
+      },
+      {
+        "id": 2,
+        "name": "Mona Barrera"
+      }
+    ],
+    "greeting": "Hello, Huffman Spears! You have 8 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "5e51b493462e6e1c83b12a2f",
+    "index": 1,
+    "guid": "70e51eee-e8c8-41a6-bb47-7ef6ea961446",
+    "isActive": false,
+    "balance": "$3,157.16",
+    "picture": "http://placehold.it/32x32",
+    "age": 32,
+    "eyeColor": "blue",
+    "name": "Kayla Lamb",
+    "gender": "female",
+    "company": "VOIPA",
+    "email": "kaylalamb@voipa.com",
+    "phone": "+1 (854) 422-3578",
+    "address": "276 Greenpoint Avenue, Lloyd, Wisconsin, 2385",
+    "about": "Elit Lorem velit tempor occaecat sunt Lorem excepteur excepteur cupidatat sunt pariatur. Tempor deserunt Lorem Lorem exercitation proident ex qui magna aliquip ex mollit minim. In excepteur ea officia ex.\r\n",
+    "registered": "2019-10-31T09:31:17 +05:00",
+    "latitude": -56.234365,
+    "longitude": 41.010863,
+    "tags": [
+      "aliqua",
+      "exercitation",
+      "esse",
+      "deserunt",
+      "tempor",
+      "consectetur",
+      "aute"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Madeleine Reed"
+      },
+      {
+        "id": 1,
+        "name": "Sheryl Romero"
+      },
+      {
+        "id": 2,
+        "name": "Langley Hunter"
+      }
+    ],
+    "greeting": "Hello, Kayla Lamb! You have 1 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "5e51b493cf8b5c36d457a142",
+    "index": 2,
+    "guid": "db6c999a-8acd-481f-b32d-2a130d30b8af",
+    "isActive": false,
+    "balance": "$2,080.11",
+    "picture": "http://placehold.it/32x32",
+    "age": 39,
+    "eyeColor": "brown",
+    "name": "Carlene Hinton",
+    "gender": "female",
+    "company": "GEOFORMA",
+    "email": "carlenehinton@geoforma.com",
+    "phone": "+1 (816) 404-3129",
+    "address": "935 Beverley Road, Roderfield, Georgia, 1795",
+    "about": "Sunt cillum mollit anim tempor culpa culpa qui do. Dolor irure incididunt ipsum ea. Deserunt reprehenderit officia reprehenderit duis. Anim incididunt pariatur nostrud exercitation deserunt in eu. Et ullamco officia do non irure ut quis aliqua amet quis esse est commodo. Nostrud reprehenderit reprehenderit do ad est ex culpa anim ex do fugiat. Eu ullamco ipsum anim qui voluptate proident ea ex do adipisicing exercitation amet culpa elit.\r\n",
+    "registered": "2018-05-20T07:56:29 +05:00",
+    "latitude": 18.443909,
+    "longitude": -129.10871,
+    "tags": [
+      "Lorem",
+      "excepteur",
+      "proident",
+      "est",
+      "tempor",
+      "sunt",
+      "et"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Jewel Guerrero"
+      },
+      {
+        "id": 1,
+        "name": "Lee Cochran"
+      },
+      {
+        "id": 2,
+        "name": "Jacklyn Wise"
+      }
+    ],
+    "greeting": "Hello, Carlene Hinton! You have 3 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "5e51b493e4d1909c0baeab0e",
+    "index": 3,
+    "guid": "bc0199aa-fa11-496e-a61f-3ea45525c9bc",
+    "isActive": true,
+    "balance": "$2,660.87",
+    "picture": "http://placehold.it/32x32",
+    "age": 23,
+    "eyeColor": "blue",
+    "name": "Lester Bell",
+    "gender": "male",
+    "company": "DENTREX",
+    "email": "lesterbell@dentrex.com",
+    "phone": "+1 (908) 594-3238",
+    "address": "136 Varet Street, Holcombe, Vermont, 4411",
+    "about": "Cupidatat eiusmod esse mollit culpa. Elit voluptate aliquip veniam velit pariatur nisi nisi irure et veniam. Et Lorem occaecat veniam sit quis consequat duis esse laboris sit ut. Pariatur anim labore incididunt mollit esse anim aute ut nulla excepteur ullamco dolore sit. Aliqua adipisicing nisi id occaecat magna commodo cillum ea occaecat. Aliqua consequat minim consectetur laborum quis proident commodo adipisicing.\r\n",
+    "registered": "2019-01-10T07:28:13 +05:00",
+    "latitude": 74.91288,
+    "longitude": -77.581011,
+    "tags": [
+      "culpa",
+      "enim",
+      "qui",
+      "consectetur",
+      "esse",
+      "ex",
+      "commodo"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Shelly Lowery"
+      },
+      {
+        "id": 1,
+        "name": "Lowery Goff"
+      },
+      {
+        "id": 2,
+        "name": "Short Gross"
+      }
+    ],
+    "greeting": "Hello, Lester Bell! You have 8 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "5e51b493cc2c16793d0d9f93",
+    "index": 4,
+    "guid": "d81df093-5a3a-491e-a6d4-cd7f2df74aff",
+    "isActive": false,
+    "balance": "$2,572.05",
+    "picture": "http://placehold.it/32x32",
+    "age": 27,
+    "eyeColor": "green",
+    "name": "Mullins Stone",
+    "gender": "male",
+    "company": "RETROTEX",
+    "email": "mullinsstone@retrotex.com",
+    "phone": "+1 (814) 532-2554",
+    "address": "467 Gaylord Drive, Dola, Alaska, 723",
+    "about": "Est veniam eu reprehenderit magna est ullamco laboris. Elit adipisicing velit reprehenderit laborum veniam voluptate velit ea laborum. Ullamco minim proident exercitation eiusmod anim incididunt consectetur officia do et sunt. Consectetur ea labore magna id commodo sint id eu quis aliquip.\r\n",
+    "registered": "2016-03-19T04:18:15 +05:00",
+    "latitude": 74.059068,
+    "longitude": 151.665718,
+    "tags": [
+      "tempor",
+      "culpa",
+      "ea",
+      "sit",
+      "qui",
+      "aliquip",
+      "magna"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Carmen William"
+      },
+      {
+        "id": 1,
+        "name": "Avery Townsend"
+      },
+      {
+        "id": 2,
+        "name": "Petersen Tucker"
+      }
+    ],
+    "greeting": "Hello, Mullins Stone! You have 7 unread messages.",
+    "favoriteFruit": "apple"
+  }
+]
